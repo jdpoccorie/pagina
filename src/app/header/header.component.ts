61b6923f07a8ae5232c07e3981034c431186a562
jdpoccorie <juandiego.poccori@gmail.com>
2024-01-17 23:11:14 -0500

    first

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,22 +1,42 @@
-import { Component , EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink , RouterOutlet} from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule , RouterLink , RouterOutlet],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
+
 export class HeaderComponent {
-  // // eventos  
+
+  isMenuOpen = false;
+  windowSize !: number;
+
+
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.updateWindowSize();
+  }
+
+  private updateWindowSize(): void {
+    this.windowSize = window.innerWidth;
+  }
+
+  // // eventos
   // @Input() occupation = '';
-  // // ahora para el hijo 
+  // // ahora para el hijo
   // @Output() incrementCoutEvent = new EventEmitter<string>();
-  
+
   // emitToParent():void{
   //   this.incrementCoutEvent.emit('Hola soy tu hijo');
   // }
-  
+
 }
